bae95b537c02e509ebfaff67d00139a9b5426943
Maxim Kott <maximkott@gmail.com>
2015-07-22 10:03:58 +0200

    Fallback for IO constants. Clean up bootstrap file.

--- src/cli.php
+++ src/cli.php
@@ -1,5 +1,17 @@
 <?php
 
+if ( ! defined('STDIN')) {
+    define('STDIN', fopen('php://stdin', 'r'));
+}
+
+if ( ! defined('STDOUT')) {
+    define('STDOUT', fopen('php://stdout', 'w'));
+}
+
+if ( ! defined('STDERR')) {
+    define('STDERR', fopen('php://stderr', 'w'));
+}
+
 if ( ! function_exists('cli_say')) {
     /**
      * Echo a terminal friendly message.
